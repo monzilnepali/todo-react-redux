7c365dc10c19ee1a3fe4a03cc218051889d576c5
manjilnepali <manjilnepali98@gmail.com>
2020-03-04 10:16:38 +0545

    adding redux dev tool

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,7 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducer from './Reducers/Todo'
 
 import loggingMiddleware from './Middleware/log.middleware'
 import apiMiddleware from './Middleware/api.middleware'
 
-export const store = createStore(reducer, applyMiddleware(loggingMiddleware, apiMiddleware));
+const composeEnhancers = typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+  }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(loggingMiddleware, apiMiddleware)
+)
+
+
+export const store = createStore(reducer, enhancer);
